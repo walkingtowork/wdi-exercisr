cdb7589481101a8023e58359b80f321accc921df
Alex Lin <alexander.lin.kremer@gmail.com>
2013-07-24 15:58:26 -0700

    basic ajax for login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,10 +9,10 @@ class UserSessionsController < ApplicationController
     respond_to do |format|
       if @user = login(params[:email], params[:password])
         format.html { redirect_back_or_to(:users, :notice => 'Login successful.') }
-        format.xml { render :xml => @user, :status => :created, :location => @user }
+        format.js
       else
         format.html { flash.now[:alert] = "Login failed."; render :action => "new" }
-        format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.js
       end
     end
   end
